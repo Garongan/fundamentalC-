14a79582d34e4b74cfe36f6d6bb469a697c5e035
alvindo <alvindo56@gmail.com>
2024-04-06 23:49:40 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// semua object atau method di c# memakai PascalCase
+
+public class Program
+{
+    public static void Main()
+    {
+
+        /* 
+        Multi Line
+         */
+
+        Console.WriteLine("Hello World"); // with new line
+        Console.Write("Hello Dunia");
+
+        Console.WriteLine();
+
+        // penulisan variabel
+        int number = 10; // explisit
+        var number1 = 10; // implisit
+
+        Console.WriteLine(number);
+        Console.WriteLine(number1);
+        
+
+        // camelCase -> variable
+        // PascalCase -> class atau method
+        // _underScore -> private field
+    }
+}
